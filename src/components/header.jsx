019f93341333986657919be49676417836956986
Aiden-10 <108913057+Aiden-10@users.noreply.github.com>
2025-06-24 17:39:56 -0400

    QoL changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,12 +6,9 @@ const Header = () => (
         <div className="container header-content">
             <Link to="/#" className="logo">AS</Link>
             <nav className="nav">
-                <Link to="/#about" className="nav-link">About</Link>
-                <Link to="/#projects" className="nav-link">Projects</Link>
-                <Link to="/#contact" className="nav-link">Contact</Link>
-                <Link to="/blog" className="nav-link">Blog</Link>
+                
             </nav>
-            <a href="https://www.github.com/Aiden-10" className="cta-button" target="_blank" rel="noopener noreferrer">Github</a>
+            <Link to="/blog" className="cta-button">Blog</Link>
         </div>
     </header>
 );
