3813b77a05d7bb4cd55a7f85be2812e6b0633933
Aiden-10 <108913057+Aiden-10@users.noreply.github.com>
2025-06-25 03:41:34 -0400

    fix

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,7 +4,7 @@ const About = () => (
     <section id="about" className="section">
         <div className="container about-grid">
             <div className="about-image-wrapper">
-                <img src="../../public/images/boy.png" alt="Aiden Portrait" className="about-image" />
+                <img src="/images/boy.png" alt="Aiden Portrait" className="about-image" />
             </div>
             <div className="about-content">
                 <h2 className="about-title">About Me</h2>
